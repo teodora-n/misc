a4bd6608936d174ec0de508b88f4855b0f4ae365
teodora-n <teodora-n@users.noreply.github.com>
2015-12-30 13:01:36 +0000

    Create calculate-pads.js

---
+++ calculate-pads.js
@@ -0,0 +1,12 @@
+  //Calculate paddings at runtime
+	function calculatePercentage(el, target, percentage){
+		var elem = $(el);
+		var targetWidth = $(target).outerWidth();
+		
+		elem.css({
+			'padding-left': (targetWidth / 100) *percentage,
+			'padding-right': (targetWidth / 100) *percentage 
+		});
+	}
+	//Call
+	calculatePercentage('.u-pad-1-12', '.site-wrap', 8.3333);
